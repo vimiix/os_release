f7fe8c74868de53067f09b6a81bf025ee1c6354b
vimiix <i@vimiix.com>
2022-07-21 16:15:28 +0800

    feat:add more os

---
+++ uos/20.1040d.sh
@@ -0,0 +1,8 @@
+PRETTY_NAME="UnionTech OS Server 20 "
+NAME="UnionTech OS Server 20 "
+VERSION_ID="20"
+VERSION="20"
+ID=UOS
+HOME_URL="https://www.chinauos.com/"
+BUG_REPORT_URL="http://bbs.chinauos.com"
+VERSION_CODENAME=fou
